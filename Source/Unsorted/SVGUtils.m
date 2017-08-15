4dab6123ba687172a3facaddf61af6ed4ede3648
spromicky <spromicky@gmail.com>
2017-08-15 14:25:18 +0300

    Add alpha channel to SVGColor.

--- Source/Unsorted/SVGUtils.m
+++ Source/Unsorted/SVGUtils.m
@@ -328,42 +328,60 @@ SVGColor SVGColorFromString (const char *string) {
 	else if (!strncmp(string, "#", 1)) {
 		const char *hexString = string + 1;
 		
-		if (strlen(hexString) == 6)
-		{
-			char r[3], g[3], b[3];
-			r[2] = g[2] = b[2] = '\0';
-			
-			strncpy(r, hexString, 2);
-			strncpy(g, hexString + 2, 2);
-			strncpy(b, hexString + 4, 2);
-			
-			color.r = strtol(r, NULL, 16);
-			color.g = strtol(g, NULL, 16);
-			color.b = strtol(b, NULL, 16);
-		}
-		else if( strlen(hexString) == 3 )
-		{
-			char r[2], g[2], b[2];
-			r[1] = g[1] = b[1] = '\0';
-			
-			strncpy(r, hexString, 1);
-			strncpy(g, hexString + 1, 1);
-			strncpy(b, hexString + 2, 1);
-			
-			color.r = strtol(r, NULL, 16);
-			color.g = strtol(g, NULL, 16);
-			color.b = strtol(b, NULL, 16);
-			
-			/** because 3-digit hex notation "F" means "FF" ... "1" means "11" ... etc */
-			color.r += color.r * 16;
-			color.g += color.g * 16;
-			color.b += color.b * 16;
-		}
-		else
-		{
-			color = SVGColorMake(0, 0, 0, 255);
-		}
-		
+        switch (strlen(hexString)) {
+            case 8:
+            {
+                char a[3];
+                a[2] = '\0';
+                
+                strncpy(a, hexString + 6, 2);
+                color.a = strtol(a, NULL, 16);
+            }
+            case 6:
+            {
+                char r[3], g[3], b[3];
+                r[2] = g[2] = b[2] = '\0';
+                
+                strncpy(r, hexString, 2);
+                strncpy(g, hexString + 2, 2);
+                strncpy(b, hexString + 4, 2);
+                
+                color.r = strtol(r, NULL, 16);
+                color.g = strtol(g, NULL, 16);
+                color.b = strtol(b, NULL, 16);
+            }
+                break;
+            case 4:
+            {
+                char a[2];
+                a[1] = '\0';
+                
+                strncpy(a, hexString + 3, 1);
+                color.a = strtol(a, NULL, 16);
+            }
+            case 3:
+            {
+                char r[2], g[2], b[2];
+                r[1] = g[1] = b[1] = '\0';
+                
+                strncpy(r, hexString, 1);
+                strncpy(g, hexString + 1, 1);
+                strncpy(b, hexString + 2, 1);
+                
+                color.r = strtol(r, NULL, 16);
+                color.g = strtol(g, NULL, 16);
+                color.b = strtol(b, NULL, 16);
+                
+                /** because 3-digit hex notation "F" means "FF" ... "1" means "11" ... etc */
+                color.r += color.r * 16;
+                color.g += color.g * 16;
+                color.b += color.b * 16;
+            }
+                break;
+            default:
+                color = SVGColorMake(0, 0, 0, 255);
+                break;
+        }
 	}
 	else {
 		color = ColorValueWithName(string);
